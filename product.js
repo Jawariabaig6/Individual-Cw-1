47fdc71ae2e68353c8298a36db0fec7a7c9a4c92
Jawairia Baig <jawariabaig6@gmail.com>
2022-11-24 11:04:01 +0400

    New commit

---
+++ product.js
@@ -0,0 +1,97 @@
+
+let products = [
+    {
+
+        id: 1001,
+        subject: "Piano Course",
+        Price: 2000,
+        Image:" images/piano class.jpg",
+        space: 10,
+        location: "Dubai",
+        
+    },
+    {
+
+        id: 1002,
+        subject: "Dance Course",
+        Price: 2000,
+        Image: "images/dance class.jpg",
+        space: 10,
+        location: "Abu Dhabi",
+    },
+    {
+
+        id: 1003,
+        subject: "Language Course",
+        Price: 2000,
+        Image: "images/language.jfif",
+        space: 10,
+        location: "Sharjah",
+    },
+    {
+
+        id: 1004,
+        subject: "SocialStudies Course",
+        Price: 2000,
+        Image: "images/social.jfif",
+        space: 10,
+        location: "Fujairah",
+    },
+    {
+
+        id: 1005,
+        subject: "Freelancing Course",
+        Price: 2000,
+        Image: "images/freelncing.png",
+        space: 10,
+        location: "Sharjah",
+    },
+    {
+
+        id: 1006,
+        subject: "Robotics Course",
+        Price: 2000,
+        Image: "images/robotics.jpg",
+        space: 10,
+        location: "Abu Dhabi",
+    },
+    {
+
+        id: 1007,
+        subject: "Business  Course",
+        Price: 2000,
+        Image: "images/business.jpg",
+        space: 10,
+        location: "Dubai",
+    },
+    {
+
+        id: 1008,
+        subject: "Math Course",
+        Price: 2000,
+        Image: "images/dance class.jpg",
+        space: 10,
+        location: "Ajman",
+    },
+    {
+
+        id: 1009,
+        subject: "Law Course",
+        Price: 2000,
+        Image: "images/language.jfif",
+        space: 10,
+        location: "Dubai",
+    },
+    {
+
+        id: 1010,
+        subject: "Reserch Course",
+        Price: 2000,
+        Image: "images/social.jfif",
+        space: 10,
+        location: "Ajman",
+    },
+  
+  
+
+];
